df7e195af18c38963f47e71603956a51cc9ca6eb
Philippe Scorsolini <p.scorsolini@gmail.com>
2023-09-28 13:35:34 +0200

    feat: support folders for functions and multiple files/folders for observed resources
    Signed-off-by: Philippe Scorsolini <p.scorsolini@gmail.com>

--- load.go
+++ load.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"io"
 	"os"
+	"path/filepath"
 
 	"k8s.io/apimachinery/pkg/util/yaml"
 
@@ -80,17 +81,48 @@ func LoadObservedResources(file string) ([]composed.Unstructured, error) {
 	return observed, nil
 }
 
-// LoadYAMLStream from the supplied file. Returns an array of byte arrays, where
-// each byte array is expected to be a YAML manifest.
-func LoadYAMLStream(file string) ([][]byte, error) {
-	f, err := os.Open(file) //nolint:gosec // Intentionally taking this as input.
+// LoadYAMLStream from the supplied file or directory. Returns an array of byte
+// arrays, where each byte array is expected to be a YAML manifest.
+func LoadYAMLStream(fileOrDir string) ([][]byte, error) {
+	var files []string
+	info, err := os.Stat(fileOrDir)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot stat file")
+	}
+	if !info.IsDir() {
+		files = append(files, fileOrDir)
+	} else {
+		files, err = filepath.Glob(filepath.Join(fileOrDir, "*.{yaml,yml}"))
+		if err != nil {
+			return nil, errors.Wrap(err, "cannot glob YAML files")
+		}
+		if len(files) == 0 {
+			return nil, errors.Errorf("no YAML files found in %q (.yaml or .yml)", fileOrDir)
+		}
+	}
+
+	out := make([][]byte, 0)
+	for i := range files {
+		o, err := LoadYAMLStreamFromFile(files[i])
+		if err != nil {
+			return nil, errors.Wrap(err, "cannot load YAML stream from file")
+		}
+		out = append(out, o...)
+	}
+
+	return out, nil
+}
+
+// LoadYAMLStreamFromFile from the supplied file. Returns an array of byte
+// arrays, where each byte array is expected to be a YAML manifest.
+func LoadYAMLStreamFromFile(file string) ([][]byte, error) {
+	out := make([][]byte, 0)
+	f, err := os.Open(file) //nolint:gosec // Taking this input is intentional.
 	if err != nil {
 		return nil, errors.Wrap(err, "cannot open file")
 	}
 	defer f.Close() //nolint:errcheck // Only open for reading.
 	yr := yaml.NewYAMLReader(bufio.NewReader(f))
-
-	out := make([][]byte, 0)
 
 	for {
 		bytes, err := yr.Read()
@@ -105,6 +137,5 @@ func LoadYAMLStream(file string) ([][]byte, error) {
 		}
 		out = append(out, bytes)
 	}
-
 	return out, nil
 }
